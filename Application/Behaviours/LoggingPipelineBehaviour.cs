705ccdaa0c02e663e42154e5ba8a1d53878c4e3c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-07 10:05:23 +0100

    feat: setting up logging pipeline behaviour

--- Application/Behaviours/LoggingPipelineBehaviour.cs
+++ Application/Behaviours/LoggingPipelineBehaviour.cs
@@ -4,15 +4,17 @@ using MediatR;
 
 namespace vsa_journey.Infrastructure.Behaviours;
 
-public class LoggingPipelineBehaviour<TRequest, TResponse>(
-    ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> logger)
-    : IPipelineBehavior<TRequest, TResponse>
+public class LoggingPipelineBehaviour<TRequest, TResponse>: IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
-    where TResponse : Result
+    where TResponse: Result
 {
 
-    private readonly ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> _logger = logger;
+    private readonly ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> _logger;
 
+    public LoggingPipelineBehaviour(ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
     public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
