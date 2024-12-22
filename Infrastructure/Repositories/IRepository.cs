430f7b3d423aeef47333054fd65afef288a009e9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-22 05:38:09 +0100

    feat: updated generic repository interface

--- Infrastructure/Repositories/IRepository.cs
+++ Infrastructure/Repositories/IRepository.cs
@@ -1,6 +1,15 @@
 namespace vsa_journey.Infrastructure.Repositories;
 
-public interface IRepository
+public interface IRepository<T> where T : class
 {
+    public Task<T> AddAsync(T entity);
+
+    public Task<T?> GetByIdAsync(Guid Id);
+
+    public Task<IEnumerable<T>> GetAllAsync(int skip, int take);
+
+    public Task UpdateAsync(T entity);
     
+    public Task DeleteAsync(T entity);
+
 }
