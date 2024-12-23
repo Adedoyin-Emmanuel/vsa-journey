fd7bb8ac5ec02342d3f411e45b24aeb921c3cd67
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-23 22:45:14 +0100

    feat: updated base response

--- Application/Responses/ApiResponse.cs
+++ Application/Responses/ApiResponse.cs
@@ -12,13 +12,12 @@ public class ApiResponse
             Data = data,
         };
     }
-
-    public object Created(object? data = null, string? message = "Resouce created successfully")
-    {
-        return BaseResponse((int)StatusCodes.Status201Created, message, data);
-    }
-    public object Success(object? data = null, string message = "Operation successful")
-    {
-        return BaseResponse((int)StatusCodes.Status200OK, message, data);
-    }
+    
+    public object Created(object? data = null, string? message = "Resouce created successfully") => BaseResponse((int)StatusCodes.Status201Created, message, data);
+    public object Success(object? data = null, string message = "Operation successful") => BaseResponse((int)StatusCodes.Status200OK, message, data);
+    public object BadRequest(string message = "Invalid request") =>  BaseResponse((int)StatusCodes.Status400BadRequest, message);
+    public object NotFound(string message = "Resource not found") => BaseResponse((int)StatusCodes.Status404NotFound, message);
+    public object Unauthorized(string message = "Unauthorized. Please login") => BaseResponse((int)StatusCodes.Status401Unauthorized, message);
+    public object Forbidden(string message = "Forbidden. Insufficient rights") => BaseResponse((int)StatusCodes.Status403Forbidden, message);
+    public object InternalServerError(string message = "An unknown error occurred") => BaseResponse((int)StatusCodes.Status500InternalServerError, message);
 }
