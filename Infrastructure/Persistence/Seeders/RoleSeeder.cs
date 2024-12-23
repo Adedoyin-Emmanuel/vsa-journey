64dd713a975954602e7d53a7f42ae1501ef4087a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-23 05:37:52 +0100

    feat: implemented role seeder

--- Infrastructure/Persistence/Seeders/RoleSeeder.cs
+++ Infrastructure/Persistence/Seeders/RoleSeeder.cs
@@ -1,6 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using vsa_journey.Domain.Constants;
+
 namespace vsa_journey.Infrastructure.Persistence.Seeders;
 
-public class RoleSeeder
+public static class RoleSeeder
 {
-    
+    public static async Task SeedRolesAsync(RoleManager<IdentityRole<Guid>> roleManager)
+    {
+        var roles = new List<string>
+        {
+            Roles.SuperAdmin,
+            Roles.Admin,
+            Roles.SalesRepresentative,
+            Roles.User
+        };
+
+        foreach (var role in roles)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(role);
+
+            if (!roleExist)
+            {
+                var newRole = new IdentityRole<Guid>(role);
+                await roleManager.CreateAsync(newRole);
+            }
+        }
+    }
 }
