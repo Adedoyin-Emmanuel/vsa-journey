8f998121aa9e0d03bf4df47a32931b92423513f7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-25 03:54:29 +0100

    feat: added Signup command

---
+++ Features/Authentication/Commands/Signup/SignupCommand.cs
@@ -0,0 +1,12 @@
+using MediatR;
+
+namespace vsa_journey.Features.Authentication.Commands.Signup;
+
+public record SignupCommand : IRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+}
