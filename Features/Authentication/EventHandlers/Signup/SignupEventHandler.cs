3fe447aa521bfdfc9b1853e5f48901b35548a80c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-25 19:21:50 +0100

    feat: implemented SignupEvent Handler

--- Features/Authentication/EventHandlers/Signup/SignupEventHandler.cs
+++ Features/Authentication/EventHandlers/Signup/SignupEventHandler.cs
@@ -1,6 +1,13 @@
+using MediatR;
+using vsa_journey.Features.Authentication.Events.Signup;
+
 namespace vsa_journey.Features.Authentication.EventHandlers.Signup;
 
-public class SignupEventHandler
+public class SignupEventHandler : INotificationHandler<SignupEvent>
 {
-    
+    public Task Handle(SignupEvent notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Signup event received");
+        Console.WriteLine($"Sending Verification Email to {notification.Email}");
+    }
 }
--- Features/Authentication/Events/Signup/SignupEvent.cs
+++ Features/Authentication/Events/Signup/SignupEvent.cs
@@ -2,13 +2,13 @@ using MediatR;
 
 namespace vsa_journey.Features.Authentication.Events.Signup;
 
-public class SigupEvent : INotification
+public class SignupEvent : INotification
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
 
-    public SigupEvent(string firstName, string lastName, string email)
+    public SignupEvent(string firstName, string lastName, string email)
     {
         FirstName = firstName;
         LastName = lastName;
