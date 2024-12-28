7946bbc267a83f5f1a593bcf58b6a9e4333937bc
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-29 00:13:45 +0100

    feat: implemented refresh token

--- Features/Authentication/RefreshToken/Command/RefreshTokenCommandHandler.cs
+++ Features/Authentication/RefreshToken/Command/RefreshTokenCommandHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using FluentResults;
 using Microsoft.AspNetCore.Identity;
 using vsa_journey.Domain.Entities.User;
+using vsa_journey.Infrastructure.Repositories.Shared.Token;
 using vsa_journey.Infrastructure.Services.Token;
 
 namespace vsa_journey.Features.Authentication.RefreshToken.Command;
@@ -11,23 +12,23 @@ public class RefreshTokenCommandHandler : IRequestHandler<RefreshAccessTokenComm
 {
     private readonly ITokenService _tokenService;
     private readonly UserManager<User> _userManager;
-    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ITokenRepository _tokenRepository;
 
 
-    public RefreshTokenCommandHandler(ITokenService tokenService, UserManager<User> userManager, IHttpContextAccessor httpContextAccessor)
+    public RefreshTokenCommandHandler(ITokenService tokenService, UserManager<User> userManager, ITokenRepository tokenRepository)
     {
         _tokenService = tokenService;
         _userManager = userManager;
-        _httpContextAccessor = httpContextAccessor;
+        _tokenRepository = tokenRepository;
     }
 
     public async Task<Result<RefreshAccessTokenResponse>> Handle(RefreshAccessTokenCommand request, CancellationToken cancellationToken)
     {
-        var userId = _httpContextAccessor?.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
-        var user = await _userManager.FindByIdAsync(userId!);
+        var validToken = await _tokenRepository.GetTokenByValueAsync(request.RefreshToken);
         
-        Console.WriteLine(userId);
-        Console.WriteLine(user);
+        if (validToken is null) return Result.Fail("Invalid request. Please try again.");
+        
+        var user = await _userManager.FindByIdAsync(validToken!.UserId.ToString());
 
         if (user == null)
         {
