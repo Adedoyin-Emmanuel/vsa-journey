f732dd6e51212b1bf31295dd65b7e17c86a7d39b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-30 13:49:15 +0100

    feat: added password reset command

---
+++ Features/Authentication/ResetPassword/Commands/ResetPasswordCommand.cs
@@ -0,0 +1,11 @@
+using MediatR;
+using FluentResults;
+
+namespace vsa_journey.Features.Authentication.ResetPassword.Commands;
+
+public sealed record ResetPasswordCommand : IRequest<Result<object>>
+{
+    public string Email { get; init; }
+    
+    public string PasswordResetCode { get; set; }
+}
