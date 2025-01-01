ff2f3cd7f03451af9def7e3f1ab6d83f3bb3d087
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-01 16:46:22 +0100

    feat: added custom email confirmation token provider options

--- Features/Authentication/Tokens/CustomEmailConfirmationTokenProvider.cs
+++ Features/Authentication/Tokens/CustomEmailConfirmationTokenProvider.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.DataProtection;
 
 namespace vsa_journey.Features.Authentication.Tokens;
 
