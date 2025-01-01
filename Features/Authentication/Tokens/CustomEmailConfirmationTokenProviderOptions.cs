1109410d2c0536bfb0bda25d82dfc8662eaf4366
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-01 17:06:49 +0100

    feat: updated email confirmation token lifespan to 3 days

--- Features/Authentication/Tokens/CustomEmailConfirmationTokenProviderOptions.cs
+++ Features/Authentication/Tokens/CustomEmailConfirmationTokenProviderOptions.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace vsa_journey.Features.Authentication.Tokens;
 
-public class CustomEmailConfirmationTokenProviderOptions
+public class CustomEmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
 {
-    
+    public CustomEmailConfirmationTokenProviderOptions()
+    {
+        Name = nameof(CustomEmailConfirmationTokenProviderOptions);
+        TokenLifespan = TimeSpan.FromDays(3);
+
+    }   
 }
