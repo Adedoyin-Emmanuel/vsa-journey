8c9f66ab3db8ca105549db597afc8daa126a379c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-04 15:33:49 +0100

    perf: refactored database seeder class

--- Infrastructure/Extensions/Initialization/DatabaseSeeder.cs
+++ Infrastructure/Extensions/Initialization/DatabaseSeeder.cs
@@ -9,13 +9,15 @@ public class DatabaseSeeder
     
     private readonly RoleSeeder _roleSeeder;
     private readonly AppDbContext _dbContext;
+    private readonly CategorySeeder _categorySeeder;
     private readonly ILogger<DatabaseSeeder> _logger;
 
-    public DatabaseSeeder(RoleSeeder roleSeeder, AppDbContext dbContext, ILogger<DatabaseSeeder> logger)
+    public DatabaseSeeder(RoleSeeder roleSeeder, CategorySeeder categorySeeder, AppDbContext dbContext, ILogger<DatabaseSeeder> logger)
     {
-        _roleSeeder = roleSeeder;
-        _dbContext = dbContext;
         _logger = logger;
+        _dbContext = dbContext;
+        _roleSeeder = roleSeeder;
+        _categorySeeder = categorySeeder;
     }
 
     public async Task UseSeedingAsync()
@@ -25,23 +27,28 @@ public class DatabaseSeeder
         try
         {
             await SeedRolesAsync();
-            
+            await SeedCategoriesAsync();
 
         }
         catch (Exception e)
         {
             _logger.LogError($"An error occurred while seeding {e}");
+            throw new Exception("An error occurred while seeding database", e);
         }
     }
     
     private async Task SeedRolesAsync()
     {
-        if (!_dbContext.Roles.Any())
-        {
-            _logger.LogInformation("Starting Role Seeding");
-            await _roleSeeder.SeedRolesAsync();
-            _logger.LogInformation("Role Seeding Completed Successfully");
-        }
+        _logger.LogInformation("Starting Role Seeding");
+        await _roleSeeder.SeedRolesAsync();
+        _logger.LogInformation("Role Seeding Completed Successfully");
+    }
+
+    private async Task SeedCategoriesAsync()
+    {
+        _logger.LogInformation("Starting Category Seeding");
+        await _categorySeeder.SeedCategoriesAsync();
+        _logger.LogInformation("Category Seeding Completed Successfully");
     }
     
     
