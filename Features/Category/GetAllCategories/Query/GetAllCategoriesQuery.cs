9c4d738e8d786352e8c0b64f5e76ebd3e3de9d91
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-05 18:36:46 +0100

    feat: implemented getAllCategories handler

--- Features/Category/GetAllCategories/Query/GetAllCategoriesQuery.cs
+++ Features/Category/GetAllCategories/Query/GetAllCategoriesQuery.cs
@@ -1,9 +1,10 @@
-using FluentResults;
 using MediatR;
+using FluentResults;
+using vsa_journey.Application.Common.PaginatedResult;
 
 namespace vsa_journey.Features.Category.GetAllCategories.Query;
 
-public sealed record GetAllCategoriesQuery : IRequest<Result<object>>
+public sealed record GetAllCategoriesQuery : IRequest<Result<PaginatedResult<Domain.Entities.Category.Category>>>
 {
     public int Take { get; set; } = 10;
     public int Skip { get; set; } = 0;
--- Features/Category/GetAllCategories/Query/GetAllCategoriesQueryHandler.cs
+++ Features/Category/GetAllCategories/Query/GetAllCategoriesQueryHandler.cs
@@ -1,27 +1,32 @@
 using FluentResults;
 using FluentValidation;
 using MediatR;
+using vsa_journey.Application.Common.PaginatedResult;
 using vsa_journey.Features.Category.GetAllCategories.Repository;
 
 namespace vsa_journey.Features.Category.GetAllCategories.Query;
 
-public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, Result<object>>
+public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, Result<PaginatedResult<Domain.Entities.Category.Category>>>
 {
     
     private readonly IValidator<GetAllCategoriesQuery> _validator;
-    private readonly ICategoryRepository _repository;
+    private readonly ICategoryRepository _categoryRepository;
     private readonly ILogger<GetAllCategoriesQueryHandler> _logger;
 
 
-    public GetAllCategoriesQueryHandler(IValidator<GetAllCategoriesQuery> validator, ICategoryRepository repository, ILogger<GetAllCategoriesQueryHandler> logger)
+    public GetAllCategoriesQueryHandler(IValidator<GetAllCategoriesQuery> validator, ICategoryRepository categoryRepository, ILogger<GetAllCategoriesQueryHandler> logger)
     {
         _validator = validator;
-        _repository = repository;
+        _categoryRepository = categoryRepository;
         _logger = logger;
     }
 
-    public async Task<Result<object>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+    public async Task<Result<PaginatedResult<Domain.Entities.Category.Category>>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        await _validator.ValidateAndThrowAsync(request, cancellationToken);
+        
+        var allCategories = await _categoryRepository.GetAllAsync(request.Skip, request.Take);
+
+        return Result.Ok(allCategories).WithSuccess("Categories fetched successfully");
     }
 }
