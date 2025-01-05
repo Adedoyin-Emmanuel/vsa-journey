5bddff34df1d51981b8bf30be1893034e42ef20b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-05 22:36:03 +0100

    chore: added file upload directory

--- Infrastructure/Services/FileUpload/FileUploadService.cs
+++ Infrastructure/Services/FileUpload/FileUploadService.cs
@@ -1,3 +1,5 @@
+using FluentResults;
+
 namespace vsa_journey.Infrastructure.Services.FileUpload;
 
 public class FileUploadService : IFileUploadService
@@ -9,13 +11,48 @@ public class FileUploadService : IFileUploadService
     {
         _logger = logger;
     }
-    public (bool, string) UploadFile(IFormFile file)
+    public Result<IUploadFileResult> UploadFile(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return Result.Fail($"File is null or empty");
+        }
+
+        var fileExtension = Path.GetExtension(file.FileName);
+        var fileExtensionCheck = CheckExtension(fileExtension);
+
+        if (fileExtensionCheck.IsFailed)
+        {
+            var failedMessage = fileExtensionCheck?.Reasons?.FirstOrDefault()?.Message ?? "Invalid file extension";
+            return Result.Fail(failedMessage);
+        }
+
+
+        try
+        {
+            var uploadPath = Path.Combine
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+
+    }
+
+    public Result<IUploadFilesResult> UploadFiles(IFormFileCollection files)
     {
         throw new NotImplementedException();
     }
 
-    public (bool, string[]) UploadFiles(IFormFileCollection files)
+    private Result CheckExtension(string extension)
     {
-        throw new NotImplementedException();
+        if (!_allowedExtensions.Contains(extension))
+        {
+            return Result.Fail($"Invalid file extension, {_allowedExtensions} are the valid file extensions");
+        }
+        
+        return Result.Ok();
     }
 }
